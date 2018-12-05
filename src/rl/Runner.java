44fca1ed41e8841bffec68603a4001f586499a65
anderson <andersonrochatavares@gmail.com>
2018-12-05 15:07:07 -0200

    now I can specify the number of matches to run

--- src/rl/Runner.java
+++ src/rl/Runner.java
@@ -18,7 +18,6 @@ import rts.units.UnitTypeTable;
 /**
  * A class to run microRTS games to train and test MetaBot
  * @author anderson
- * TODO read the number of matches from a config file
  * TODO use the methods to save and load weights
  */
 public class Runner {
@@ -35,52 +34,72 @@ public class Runner {
         prop.load(is);
 		GameSettings settings = GameSettings.loadFromConfig(prop);
 		System.out.println(settings);
+		is.close();
 		
 		UnitTypeTable utt = new UnitTypeTable(settings.getUTTVersion(), settings.getConflictPolicy());
-        PhysicalGameState pgs = null;
-		pgs = PhysicalGameState.load(settings.getMapLocation(), utt);
-
-        GameState gs = new GameState(pgs, utt);
-        //int PERIOD = 20;
-        boolean gameover = false;
-        
         AI ai1 = loadAI(settings.getAI1(), utt, prop);
         AI ai2 = loadAI(settings.getAI2(), utt, prop);
-
-        //JFrame w = PhysicalGameStatePanel.newVisualizer(gs,640,640,settings.isPartiallyObservable(),
-        //                                                PhysicalGameStatePanel.COLORSCHEME_BLACK);
-
-        //long nextTimeToUpdate = System.currentTimeMillis() + PERIOD;
-        do{
-            //if (System.currentTimeMillis()>=nextTimeToUpdate) {
-                if (settings.isPartiallyObservable()) {
-                    PlayerAction pa1 = ai1.getAction(0, new PartiallyObservableGameState(gs,0));
-                    PlayerAction pa2 = ai2.getAction(1, new PartiallyObservableGameState(gs,1));            
-                    gs.issueSafe(pa1);
-                    gs.issueSafe(pa2);
-                } else {
-                    PlayerAction pa1 = ai1.getAction(0, gs);
-                    PlayerAction pa2 = ai2.getAction(1, gs);
-                    gs.issueSafe(pa1);
-                    gs.issueSafe(pa2);
-                }
-
-                // simulate:
-                gameover = gs.cycle();
-           //     w.repaint();
-           //     nextTimeToUpdate+=PERIOD;
-           /* } else {
-                try {
-                    Thread.sleep(1);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }*/
-        }while(!gameover && gs.getTime()<settings.getMaxCycles());
-        ai1.gameOver(gs.winner());
-        ai2.gameOver(gs.winner());    
-        System.out.println("Finished.");
+        
+        int numGames = Integer.parseInt(prop.getProperty("num_games", "1"));
+        
+        for(int i = 0; i < numGames; i++){
+        	headlessMatch(ai1, ai2, settings.getMapLocation(), settings.getMaxCycles(), utt, settings.isPartiallyObservable());
+        	System.out.print(String.format("\rMatch %8d finished.", i+1));
+        }
+        
+        System.out.println("\nExecuted " + numGames + " matches.");
 	}
+	
+	/**
+	 * Runs a match between two AIs, in the specified map and parameters without the GUI.
+	 * @param ai1
+	 * @param ai2
+	 * @param mapFile
+	 * @param timeLimit
+	 * @param types
+	 * @param partiallyObservable
+	 * @throws Exception
+	 */
+    public static void headlessMatch(AI ai1, AI ai2, String mapFile, int timeLimit, UnitTypeTable types, boolean partiallyObservable) throws Exception{
+        PhysicalGameState pgs;
+		try {
+			pgs = PhysicalGameState.load(mapFile, types);
+		} catch (Exception e) {
+			System.err.println("Error while loading map from file: " + mapFile);
+			e.printStackTrace();
+			System.err.println("Aborting match execution...");
+			return;
+		}
+
+        GameState state = new GameState(pgs, types);
+        
+        boolean gameover = false;
+    	
+        while (!gameover && state.getTime() < timeLimit) {
+        	// initializes state equally for the players 
+        	GameState player1State = state; 
+        	GameState player2State = state; 
+        	
+        	// places the fog of war if the state is partially observable
+        	if (partiallyObservable) {
+        		player1State = new PartiallyObservableGameState(state, 0);
+        		player2State = new PartiallyObservableGameState(state, 0);
+        	}
+        	
+        	// retrieves the players' actions
+        	PlayerAction player1Action = ai1.getAction(0, player1State);
+        	PlayerAction player2Action = ai2.getAction(1, player2State);
+			
+        	// issues the players' actions
+			state.issueSafe(player1Action);
+			state.issueSafe(player2Action);
+
+			// runs one cycle of the game
+			gameover = state.cycle();
+		} 
+		ai1.gameOver(state.winner());
+		ai2.gameOver(state.winner());
+    }
 
 	/**
 	 * Loads an {@link AI} according to its name, using the provided UnitTypeTable.
