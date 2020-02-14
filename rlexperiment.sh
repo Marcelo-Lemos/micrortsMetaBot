e3d7d1a8d1c87acdc5f4396d9c3afd8ba4ec8377
Anderson <andersonrochatavares@gmail.com>
2020-02-14 17:51:51 -0300

    had to manually set the classpath in rlexperiment.sh :(

--- rlexperiment.sh
+++ rlexperiment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-classpath=.:bin:lib/*
+classpath=.:bin:lib/microrts.jar:lib/commons-cli-1.4.jar:lib/log4j-api-2.11.1.jar
+classpath="$classpath:lib/log4j-core-2.11.1.jar:lib/jdom.jar:lib/ufv.jar:lib/capivara.jar"
 
 echo "Launching experiment..."
 
